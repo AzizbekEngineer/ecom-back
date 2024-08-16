e663de3713ff0833c0e79148b17750cd78a719fc
AzizbekEngineer <azizbekengineer@gmail.com>
2024-08-16 12:31:03 +0500

    add

--- routers/index.js
+++ routers/index.js
@@ -25,7 +25,7 @@ router.patch("/api/update/category/:id", CategoryController.update);
 //products
 router.get("/api/products", ProductsController.get);
 router.get("/api/products/category/:id", ProductsController.getCategory);
-router.delete("/api/delete/product:id", ProductsController.getCategory);
+router.delete("/api/delete/product/:id", ProductsController.delete);
 router.post(
   "/api/create/product",
   [auth, upload.array("photos")],
